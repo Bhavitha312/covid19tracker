d77949587d09de14220f8134c5cdd1dcb7e53782
bhavitha312 <bhavitha312.d@gmail.com>
2023-04-06 18:55:55 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [data, setData]=useState([]);
+
+  useEffect(()=>{
+    fetch('https://data.covid19india.org/data.json').then(
+      response=>response.json()
+    ).then(jsondata=>setData(jsondata.statewise))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>INDIA COVID-19 CASES STATEWISE</h1>
+        <table className='table'>
+          <thead className='thead-dark'>
+            <tr>
+              <th>state</th>
+              <th>confirmed</th>
+              <th>recovered</th>
+              <th>deaths</th>
+              <th>active</th>
+              <th>lastupdatedtime</th>
+            </tr>
+          </thead>
+          <tbody>
+           {data.map(item =>{
+            return(
+              <tr>
+              <td>{item.state}</td>
+              <td>{item.confirmed}</td>
+              <td>{item.recovered}</td>
+              <td>{item.deaths}</td>
+              <td>{item.active}</td>
+              <td>{item.lastupdatedtime}</td>
+              </tr>
+            )
+           })}
+          </tbody>
+        </table>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
